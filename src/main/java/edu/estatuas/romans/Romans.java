2c3460d1f0d1bfb3b50331bf2785dc6374fc35e1
Pontax02 <ppontanilla07@gmail.com>
2025-05-15 13:32:27 +0200

    :sparkles: feat(Romans): Añadir la clase Romans donde se implementara la lógica de toDecimal

---
+++ src/main/java/edu/estatuas/romans/Romans.java
@@ -0,0 +1,18 @@
+package edu.estatuas.romans;
+
+public class Romans {
+
+    final String romanNumber;
+
+
+    public Romans(String romanNumber) {
+        this.romanNumber = romanNumber;
+    }
+
+
+
+    public short toDecimal() {
+        return 0;
+    }
+
+}
